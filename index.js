889cac817b501588323830bbc4c2711384d504f9
Unaisnml <unaisnml@gmail.com>
2024-03-16 22:07:05 +0530

    api for delete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { MongoClient, ServerApiVersion } from 'mongodb';
+import { MongoClient, ObjectId, ServerApiVersion } from 'mongodb';
 
 dotenv.config()
 const app = express()
@@ -53,7 +53,26 @@ async function run() {
         }
     })
 
-  
+    //get jobs by email
+    app.get('/my-job/:email',async(req, res)=>{
+      try {
+           // console.log(req.params.email);
+      const jobs = await jobsCollections.find({postedBy: req.params.email}).toArray()
+      res.json(jobs)
+      } catch (error) {
+        console.log("Error fetching jobs:", error);
+        res.status(500).json({ error: "Failed to fetch jobs" });
+      }
+   
+    })
+
+    //Delete job
+    app.delete('/job/:id',async(req, res)=>{
+      const id = req.params.id
+      const filter = {_id: new ObjectId(id)}
+      const result = await jobsCollections.deleteOne(filter)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
@@ -67,7 +86,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Hello Unais!')
+  res.send('Hello ')
 })
 
 app.listen(port, () => {
